5c9055244307815fa4d94c181274e3c40ce2075c
M Shekhar Dora <63382577+Shekhar403@users.noreply.github.com>
2023-04-08 19:40:27 +0530

    update 6

--- app.js
+++ app.js
@@ -20,43 +20,41 @@ app.post("/", function(req, res){
     var fName = req.body.fName;
     var lName = req.body.lName;
     var email = req.body.email;
-
-    res.send(fName + " " + lName + " " + email);
  
-// //Setting up MailChimp
-//     mailchimp.setConfig({
-// //write your api key 
-//       apiKey: "41dbc74600d116fc5d2e83f3a760db18-us21",
-// //and server prefix
-//       server: "us21",
-//     });
+//Setting up MailChimp
+    mailchimp.setConfig({
+//write your api key 
+      apiKey: "41dbc74600d116fc5d2e83f3a760db18-us21",
+//and server prefix
+      server: "us21",
+    });
     
-// // write your audience/list_id here
-//     const list_id = "cc951b2e83";
+// write your audience/list_id here
+    const list_id = "cc951b2e83";
  
-// // All the below code are from mailchimp docs
-//     const run = async () => {
-//         const response = await mailchimp.lists.batchListMembers(list_id, {
-//           members: [{
-//             email_address:email,
-//             status:"subscribed",
-//             merge_fields:{
-//                 FNAME:fName,
-//                 LNAME:lName
-//             }
-//           }],
-//         });
+// All the below code are from mailchimp docs
+    const run = async () => {
+        const response = await mailchimp.lists.batchListMembers(list_id, {
+          members: [{
+            email_address:email,
+            status:"subscribed",
+            merge_fields:{
+                FNAME:fName,
+                LNAME:lName
+            }
+          }],
+        });
         
-//         if (response.errors.length === 0) {
-//             // res.send(response.new_members[0].status);
-//             res.sendFile(__dirname + "/success.html");
-//         }
-//         else {
-//             res.sendFile(__dirname + "/failure.html");
-//         }
-//       };
+        if (response.errors.length === 0) {
+            // res.send(response.new_members[0].status);
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+      };
       
-//       run();   
+      run();   
 });
  
 app.post("/failure", function(req, res){
